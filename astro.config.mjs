34641c58c3d6c5b6f8a7baa15ed9e9e4fe548433
ser0125 <sergio.llanos.garcia@gmail.com>
2024-11-11 20:50:32 -0500

    Add a new yml configuration to deploy using github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://ser0125.github.io',
+    base: 'sergiollanos.dev',
+});
