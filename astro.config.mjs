f4954ac5cac42cecdee0acd5ddbea0ae3e350ea8
ser0125 <sergio.llanos.garcia@gmail.com>
2024-11-17 21:43:08 -0500

    Add lottie animation and some changes on the fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ser0125.github.io',
-    base: 'sergiollanos.dev',
+  site: "https://ser0125.github.io",
+  base: "sergiollanos.dev",
+  integrations: [lottie()],
 });
